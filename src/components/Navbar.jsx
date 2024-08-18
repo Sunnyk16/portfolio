dcbc9b22147610e27d7910481477b0fa628464bf
Sunnyk16 <kumarsunny1246@gmail.com>
2024-08-18 18:18:44 +0530

    resume added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,27 @@ function Navbar() {
     <div className='overflow-x-hidden text-neutral-900  antialiased selection:to-blue-600 selection:text-blue-300'>
       {/* navcomponet */}
       <nav className="  mb-5 flex items-center justify-between py-6">
+        {/* <div className="flex flex-shrink-0 items-center">
+          <img src={logo} alt=""/>
+          
+
+        </div> */}
         <div className="flex flex-shrink-0 items-center">
           {/* <img src={logo} alt=""/> */}
-
+          <a
+            href="https://drive.google.com/file/d/16MExcnDePwZPrCwXmqf0BbwDcFjf7HeE/view"
+            target="_blank"
+            rel="noreferrer"
+          >
+            
+            <button
+              className="border border-neutral-800 shadow-sm shadow-slate-200  text-white font-medium py-2 px-4 rounded-lg hover:bg-white hover:text-black transition duration-200"
+            >
+              Resume
+            </button>
+          </a>
         </div>
+
 
         <div className='m-8 flex items-center justify-center gap-4 text-2xl text-white'>
           {/* <a className='hover:text-blue-800' href="https://www.linkedin.com/in/sunny-kumar16/" target="_blank" rel="noreferrer "><FaLinkedin /></a>
