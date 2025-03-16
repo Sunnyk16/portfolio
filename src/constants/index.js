575a4cbc2a7869bd783f5a1892a75d3856249f7e
Sunnyk16 <kumarsunny1246@gmail.com>
2025-03-17 01:31:25 +0530

    update HERO_CONTENT to reflect enhanced skills and technologies

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,12 @@ import project2 from "../assets/projects/project-2.png";
 import project3 from "../assets/projects/project-3.png";
 
 
-export const HERO_CONTENT = `Enthusiastic Full Stack Developer specializing in front-end development with a knack for technologies such as HTML, CSS, Tailwind CSS, Bootstrap, React.js, and JavaScript. Strong working knowledge of MongoDB and Node.js. Ready for hands-on project experiences in a dynamic development environment to apply and further my skills.`;
+export const HERO_CONTENT = `Enthusiastic and detail-oriented Full-Stack Developer with expertise in front-end and back-end technologies, including
+
+HTML, CSS, Next.js, React.js, Node.js, MongoDB, and Express.js. Proficient in building dynamic, responsive, and user-
+friendly web applications using modern frameworks like Tailwind CSS, Bootstrap, and Framer Motion. Eager to contribute
+
+to innovative projects in a collaborative environment while further enhancing technical and problem-solving skills.`;
 
 export const ABOUT_TEXT = `Passionate Full Stack Developer with a strong focus on front-end technologies, including HTML, CSS, Tailwind CSS, Bootstrap, and React.js. Proficient in MongoDB and Node.js, with a commitment to applying and expanding my skills in a dynamic and collaborative environment. Ready to tackle hands-on projects and drive impactful results.`;
 
