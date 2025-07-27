d4fdac0988b35f0b8ddc5f995a6bcdcac6378376
Sunnyk16 <kumarsunny1246@gmail.com>
2025-07-27 18:40:09 +0530

    refactor: update Seo component to use static title and description for improved SEO

--- src/components/SEO/Seo.jsx
+++ src/components/SEO/Seo.jsx
@@ -3,10 +3,17 @@ import { Helmet } from 'react-helmet-async'
 
 function Seo({ title, description } ) {
   return (
-    <Helmet  titleTemplate="Sunny Kumar">
-        <title>{title}</title>
-        <meta name="description" content={description} />
-    </Helmet>
+    // <Helmet  titleTemplate="Sunny Kumar">
+    //     <title>{title}</title>
+    //     <meta name="description" content={description} />
+    // </Helmet>
+    <Helmet>
+  <title>Full Stack Developer | UI Designer | Code Debugger | Sunny Kumar</title>
+  <meta name="description" content="Sunny Kumar is a Full Stack Developer specializing in UI design and clean, bug-free code. Explore projects built with React, Tailwind, Node, and MongoDB." />
+  <meta name="robots" content="index, follow" />
+  <link rel="canonical" href="https://sunnykumar.me/" />
+</Helmet>
+
   )
 }
 
