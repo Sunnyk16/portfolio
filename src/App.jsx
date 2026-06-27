c2a4e750ea33df1ea43e95b40ba279b3570482f3
Sunnyk16 <kumarsunny1246@gmail.com>
2026-06-27 22:34:56 +0530

    reveretd the changes

--- src/App.jsx
+++ src/App.jsx
@@ -13,25 +13,25 @@ function App() {
   return (
     <main className='overflow-x-hidden  text-neutral-300 antialiased selection:to-blue-600 selection:text-blue-300 '>
       <div className='fixed top-0 -z-10 h-full w-full'>
-      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
-        
+        <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+
         </div>
-        <h1>Sunny Kumar</h1>
+
 
       </div>
-      
+
 
       <div className='container mx-auto px-8 '>
-      <Navbar />
-      <HeroSection/>
-      <About/>
-      <Technology/>
-      <Experience/>
-      <Projects/>
-      <Conatct/>
+        <Navbar />
+        <HeroSection />
+        <About />
+        <Technology />
+        <Experience />
+        <Projects />
+        <Conatct />
       </div>
 
-      
+
 
     </main>
   )
